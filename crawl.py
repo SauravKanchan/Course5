e62a1a011cabfe7fd148107b811081ba22f551e1
SauravKanchan <sauravnk30@gmail.com>
2019-04-17 14:51:56 +0530

    Add asin reviwer_name review_title in review crawl

--- crawl.py
+++ crawl.py
@@ -17,7 +17,7 @@ class AmazonReviewsSpider(scrapy.Spider):
     start_urls = []
 
     # Creating list of urls to be scraped by appending page number a the end of base url
-    for i in range(1, 300):
+    for i in range(1, 100):
         start_urls.append(myBaseUrl + str(i))
 
     # Defining a Scrapy parser
@@ -29,12 +29,21 @@ class AmazonReviewsSpider(scrapy.Spider):
 
         # Collecting user reviews
         comments = data.css('.review-text')
+
+        # review_votes = data.css('.cr-vote-text')
+
+        reviewer_name = data.css('.a-profile-name')
+
+        review_title = data.css('.review-title')
         count = 0
 
-        # Combining the results
         for review in star_rating:
             yield {
+                'review_title': (''.join(review_title[count].xpath(".//text()").extract())).replace("\n", ""),
+                'reviewer_name': (''.join(reviewer_name[count].xpath(".//text()").extract())).replace("\n", ""),
                 'stars': (''.join(review.xpath('.//text()').extract())).replace(" out of 5 stars", ""),
                 'comment': (''.join(comments[count].xpath(".//text()").extract())).replace("\n", ""),
+                'asin': response.request.url.split('/')[4],
+                # 'review': (''.join(review_votes[count].xpath(".//text()").extract())).replace("\n", "").replace(' people found this helpful',''),
             }
             count = count + 1
